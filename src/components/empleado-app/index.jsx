a0e7d6fdc2484f4d2bc72c32b16384d2cac93455
xavi <xavi.hidalgo@apiumtech.com>
2016-02-14 14:31:34 +0100

    fix not existing API images

--- src/components/empleado-app/index.jsx
+++ src/components/empleado-app/index.jsx
@@ -18,14 +18,14 @@ class EmpleadoApp extends React.Component {
     //  })
 		let empleados = [
 			{ id: 1, fullName: "Laya Dueñas", title: "CEO", department: "Business", pic: "empleado01.png" },
-			{ id: 2, fullName: "Astryd Vallés", title: "CMO", department: "Marketing", pic: "empleado02.png" },
-			{ id: 3, fullName: "Shantell Meza", title: "CFO", department: "Business", pic: "empleado03.png" },
-			{ id: 4, fullName: "Sergio Ocampo", title: "CTO", department: "Engineering", pic: "empleado04.png" },
-			{ id: 5, fullName: "Ares Jiménez", title: "Art Director", department: "Marketing", pic: "empleado05.png" },
-			{ id: 6, fullName: "Marta Pérez", title: "Frontend Dev", department: "Engineering", pic: "empleado06.png" },
-			{ id: 7, fullName: "Ellen Balderas", title: "Digital Strategist", department: "Marketing", pic: "empleado07.png" },
-			{ id: 8, fullName: "Cynthia Valentín", title: "Backend Dev", department: "Engineering", pic: "empleado08.png" },
-			{ id: 9, fullName: "Bernard Jung", title: "DevOps Engineer", department: "Engineering", pic: "empleado09.png" },
+			{ id: 2, fullName: "Astryd Vallés", title: "CMO", department: "Marketing", pic: "empleado01.png" },
+			{ id: 3, fullName: "Shantell Meza", title: "CFO", department: "Business", pic: "empleado01.png" },
+			{ id: 4, fullName: "Sergio Ocampo", title: "CTO", department: "Engineering", pic: "empleado01.png" },
+			{ id: 5, fullName: "Ares Jiménez", title: "Art Director", department: "Marketing", pic: "empleado01.png" },
+			{ id: 6, fullName: "Marta Pérez", title: "Frontend Dev", department: "Engineering", pic: "empleado01.png" },
+			{ id: 7, fullName: "Ellen Balderas", title: "Digital Strategist", department: "Marketing", pic: "empleado01.png" },
+			{ id: 8, fullName: "Cynthia Valentín", title: "Backend Dev", department: "Engineering", pic: "empleado01.png" },
+			{ id: 9, fullName: "Bernard Jung", title: "DevOps Engineer", department: "Engineering", pic: "empleado01.png" },
 
 		]
 		this.setState({ empleados: empleados });
--- src/components/empleado-avatar/index.jsx
+++ src/components/empleado-avatar/index.jsx
@@ -7,7 +7,7 @@ class EmpleadoAvatar extends React.Component {
 
     return (
       <figure className="media-left">
-        <img className="media-object" width="64px" src={`http://taller-angular.carlosazaustre.es/asset/${this.props.picture}`} />
+        <img className="media-object" width="64px" src={`img/${this.props.picture}`} />
       </figure>
     )
   }
