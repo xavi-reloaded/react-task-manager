8f89536d411a5d13fa43d5f8455734a85379632c
xavi <xavi.hidalgo@apiumtech.com>
2016-02-16 08:33:27 +0100

    add todo

--- src/components/todo/model/index.jsx
+++ src/components/todo/model/index.jsx
@@ -1,28 +1,24 @@
 
-class TodoModel {
-
-
-	getTodos() {
-		//fetch('http://myURL')
-		//  .then((response) => {
-		//    return response.json()
-		//  })
-		//  .then((empleados) => {
-		//    this.setState({ todos: todos })
-		//  })
-		let todos = [
+var TodoModel = function (todos) {
+	todos = todos || [
 			{ id: 1, title: "Llamar a Mr Diguiger", active: true },
 			{ id: 2, title: "Reunión con Richard", active: true },
 			{ id: 3, title: "Estimar backlog", active: true },
 			{ id: 4, title: "Enviar email", active: true }
-		]
+		];
+	this.todos = todos;
+};
 
-		return todos;
-	}
+TodoModel.prototype.getTodos = function () {
+	return this.todos;
+};
 
-	getViewModel(){
-		return { todos: {a:'174px', b:'none', c:1} };
-	}
+TodoModel.prototype.getViewModel = function () {
+	return { a:'174px', b:'none', c:1, showTodo:'show' };
+}
+
+TodoModel.prototype.inputTodo = function(text) {
+	this.todos.push({ id: this.todos.length + 1, title: text, active: true });
 }
 
 export default TodoModel
--- src/components/todo/todo-app/index.jsx
+++ src/components/todo/todo-app/index.jsx
@@ -7,30 +7,43 @@ class TodoApp extends React.Component {
   constructor(props) {
     super(props)
 		this.model = new TodoModel;
-		this.viewModel = this.model.getViewModel()
+		this.switchTodo = this.switchTodo.bind(this);
+		this.inputTodo = this.inputTodo.bind(this);
   }
 
 	componentWillMount() {
-		this.setState({ todos: this.model.getTodos() });
+		this.setState({ todos: this.model.getTodos(), view: this.model.getViewModel() });
+	}
+
+	switchTodo () {
+		this.setState( (this.state.view.showTodo == 'show') ? {view:{showTodo:''}} : {view:{showTodo:'show'}} );
+	}
+
+	inputTodo (event) {
+		if (event.key === 'Enter') {
+			this.model.inputTodo(event.target.value);
+			this.setState(this.model.getTodos());
+			event.target.value='';
+		}
+
 	}
 
   render() {
 		if (this.state.todos.length > 0) {
 			return (
 
-				<div id="todo" className="show" style={{opacity: this.viewModel.todos.c}}>
-					<div className="pane todo-pane todo-list" style={{'max-height': this.viewModel.todos.a }}>
+				<div id="todo" className={this.state.view.showTodo} style={{opacity: this.state.view.c}}>
+					<div className="pane todo-pane todo-list" style={{'max-height': this.state.view.a }}>
 						<div className="todo-list-header">
 							<div id="todo-count">{this.state.todos.length + ' to do'}</div>
 						</div>
 
 						<TodoList todos={this.state.todos} />
 
-						<input id="todo-new" className="todo-input" type="text" placeholder="New todo"/>
+						<input id="todo-new" className="todo-input" type="text" placeholder="New todo" onKeyPress={this.inputTodo}/>
 					</div>
 					<span id="todo-remaining"></span>
-					<span className="toggle todo-toggle">Todo</span>
-					<li className="placeholder" style={{display: this.viewModel.todos.b}}></li>
+					<span className="toggle todo-toggle" onClick={this.switchTodo}>Todo</span>
 				</div>
 
 			)
